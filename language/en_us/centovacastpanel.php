73fa66b1300938be70595830a1bf546a2849140b
Jacob Braun <36256725+Gigaion@users.noreply.github.com>
2022-11-13 11:40:36 -0800

    Initial Commit
    Initial Commit. Code removed of purchase requirement and is now open sourced as untested.

---
+++ language/en_us/centovacastpanel.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * en_us language for the Centovacastpanel module.
+ */
+// Basics
+$lang['Centovacastpanel.name'] = 'CentovaCastPanel';
+$lang['Centovacastpanel.description'] = 'Allows you to offer shoutcast, icecast, streaming media hosting, AutoDJ, radio reseller from your Dedicated or VPS server.';
+$lang['Centovacastpanel.module_row'] = 'Server';
+$lang['Centovacastpanel.module_row_plural'] = 'Servers';
+$lang['Centovacastpanel.module_group'] = 'Server Group';
+$lang['Centovacastpanel.tab_stats'] = 'Radio Login Information';
+$lang['Centovacastpanel.tab_client_actions'] = 'Change Password';
+$lang['Centovacastpanel.submit'] = 'Submit';
+
+// Tab Stats
+$lang['Centovacastpanel.tab_stats.info_heading.field'] = 'Field';
+$lang['Centovacastpanel.tab_stats.info_heading.value'] = 'Value';
+$lang['Centovacastpanel.tab_stats.info.radiousername'] = 'Radio Username';
+$lang['Centovacastpanel.tab_stats.info.radiopassword'] = 'Radio Password';
+$lang['Centovacastpanel.tab_stats.info.ipaddress'] = 'Radio IP';
+$lang['Centovacastpanel.tab_stats.info.hostname'] = 'Radio Hostname';
+$lang['Centovacastpanel.tab_stats.info.loginlink'] = 'Radio Login Link';
+$lang['Centovacastpanel.tab_stats.info.loginbutton'] = 'Radio Login';
+$lang['Centovacastpanel.tab_stats.info.passwordwarning1'] = 'Your radio password is the admin password!';
+$lang['Centovacastpanel.tab_stats.info.passwordwarning2'] = 'If you changed it manually in CentovaCast the above password will not be correct and login will fail.';
+$lang['Centovacastpanel.tab_stats.info.passwordwarning3'] = 'You may reset your password using the Change Password feature if you do not remember the password.';
+// Tab Actions
+$lang['Centovacastpanel.tab_client_actions.change_password'] = "Change Passwords";
+$lang['Centovacastpanel.tab_client_actions.radiopassword'] = "New Radio Password";
+
+// Module management
+$lang['Centovacastpanel.add_module_row'] = 'Add Server';
+$lang['Centovacastpanel.manage.module_rows_title'] = 'Centovacastpanel Servers';
+$lang['Centovacastpanel.manage.module_rows_heading.name'] = 'Server Label';
+$lang['Centovacastpanel.manage.module_rows_heading.ipaddress'] = 'IP Address';
+$lang['Centovacastpanel.manage.module_rows_heading.hostname'] = 'Hostname';
+$lang['Centovacastpanel.manage.module_rows_heading.options'] = 'Options';
+$lang['Centovacastpanel.manage.module_groups_heading.name'] = 'Group Name';
+$lang['Centovacastpanel.manage.module_groups_heading.servers'] = 'Server Count';
+$lang['Centovacastpanel.manage.module_groups_heading.options'] = 'Options';
+$lang['Centovacastpanel.manage.module_rows.edit'] = 'Edit';
+$lang['Centovacastpanel.manage.module_groups.edit'] = 'Edit';
+$lang['Centovacastpanel.manage.module_rows.delete'] = 'Delete';
+$lang['Centovacastpanel.manage.module_groups.delete'] = 'Delete';
+$lang['Centovacastpanel.manage.module_rows.confirm_delete'] = 'Are you sure you want to delete this server?';
+$lang['Centovacastpanel.manage.module_groups.confirm_delete'] = 'Are you sure you want to delete this server group?';
+$lang['Centovacastpanel.manage.module_rows_no_results'] = 'There are no servers.';
+$lang['Centovacastpanel.manage.module_groups_no_results'] = 'There are no server groups.';
+
+$lang['Centovacastpanel.order_options.first'] = 'First non-full server';
+
+// Add-Edit Admin Module Page
+$lang['Centovacastpanel.add_row.addserver'] = 'Add Centovacastpanel Server';
+$lang['Centovacastpanel.add_row.basic_title'] = 'Basic Settings';
+$lang['Centovacastpanel.add_row.add_btn'] = 'Add Server';
+
+$lang['Centovacastpanel.edit_row.editserver'] = 'Edit Centovacastpanel Server';
+$lang['Centovacastpanel.edit_row.basic_title'] = 'Basic Settings';
+$lang['Centovacastpanel.edit_row.add_btn'] = 'Edit Server';
+
+$lang['Centovacastpanel.row_meta.server_name'] = 'CentovaCastPanel Name Label';
+$lang['Centovacastpanel.row_meta.ipaddress'] = 'CentovaCastPanel Server IP Address';
+$lang['Centovacastpanel.row_meta.hostname'] = 'CentovaCastPanel Server Hostname';
+$lang['Centovacastpanel.row_meta.adminusername'] = 'CentovaCastPanel Username (Admin Account)';
+$lang['Centovacastpanel.row_meta.adminapikey'] = 'CentovaCastPanel Admin Password';
+$lang['Centovacastpanel.row_meta.usessl'] = 'Use SSL (required for certain CentovaCastPanel features)';
+$lang['Centovacastpanel.row_meta.useproxy'] = 'Use IPv6 to IPv4 Proxy (Useful if Blesta uses IPv6 but CentovaCastPanel does not)';
+$lang['Centovacastpanel.row_meta.hostname'] = 'CentovaCastPanel Domain Hostname';
+
+// Service fields
+$lang['Centovacastpanel.service_field.radiousername'] = 'CentovaCastPanel Radio Username';
+$lang['Centovacastpanel.service_field.radiopassword'] = 'CentovaCastPanel Radio Password';
+$lang['Centovacastpanel.service_field.radiousername.tooltip'] = 'The username will only be updated locally within Blesta';
+$lang['Centovacastpanel.service_field.radiopassword.tooltip'] = 'The password will be updated on CentovaCastPanel';
+$lang['Centovacastpanel.service_field.module_package_error'] = 'No radio package found on specified server! You must first add a package to CentovaCastPanel.';
+//$lang['Centovacastpanel.service_field.module_configoptionmessage'] = 'Use Configurable Option Instead (Recommended)';
+
+// Service info
+$lang['Centovacastpanel.service_info.ipaddress'] = 'IP Address';
+$lang['Centovacastpanel.service_info.hostname'] = 'Hostname';
+$lang['Centovacastpanel.service_info.radiousername'] = 'CentovaCastPanel Radio Username';
+$lang['Centovacastpanel.service_info.radiopassword'] = 'CentovaCastPanel Radio Password';
+$lang['Centovacastpanel.service_info.options'] = 'Options';
+$lang['Centovacastpanel.service_info.option_login'] = 'Log in';
+
+// Errors
+$lang['Centovacastpanel.!error.adminusername_valid'] = 'You must enter a Username.';
+$lang['Centovacastpanel.!error.server_name_valid'] = 'You must enter a Server Label.';
+$lang['Centovacastpanel.!error.ipaddress_valid'] = 'The IP Address appears to be invalid.';
+$lang['Centovacastpanel.!error.hostname_valid'] = 'The Hostname Address appears to be invalid.';
+$lang['Centovacastpanel.!error.adminapikey_valid'] = 'The password credentials appear to be invalid.';
+$lang['Centovacastpanel.!error.api.internal'] = 'An internal error occurred, or the server did not respond to the request.';
+$lang['Centovacastpanel.!error.module_row.missing'] = 'An internal error occurred. The module row is unavailable.';
+
+
+$lang['Centovacastpanel.!error.radiousername.empty'] = "Username can't be empty.";
+$lang['Centovacastpanel.!error.radiopassword.valid'] = 'Password must be at least 8 characters in length.';
+
+//Package Fields
+//$lang['Centovacastpanel.package_fields.package'] = 'Package';
+//$lang['Centovacastpanel.!error.meta[package].valid'] = 'Empty CentovaCastPanel Package Set.';
